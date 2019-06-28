b62bc0f242c74493889a1b6ffb457d89bde47a3d
pak-405 <52315198+pak-405@users.noreply.github.com>
2019-06-28 15:22:14 +0000

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const port = 80
 const server = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'})
   response.write('Hello World\n')
+  response.write('<h1>Azure Kubernetes</h1> <br/> <h2>My name is Peter Adomire.</h2>\n')
   response.end('Version: ' + process.env.NODE_VERSION + '\n')
 })
 
